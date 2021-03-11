5ff4a36e7c4c698c777b7e5f69d62acab737376d
Anatoliy Gargash <kabantolyan@gmail.com>
2021-03-11 21:46:02 +0200

    finished click n prepared form

--- src/js/todo.js
+++ src/js/todo.js
@@ -1,19 +1,30 @@
 import { v4 as uuidv4 } from 'uuid';
 
-// const btn = document.querySelector('.workplace__add');
-// const parent = document.querySelector('.workplace__inner');
+const btn = document.querySelector('.workplace__add');
+const parent = document.querySelector('.workplace__inner');
 
-// const generate = (value) => {
-//   return `
-//   <li class="workplace__item" data-key="${uuidv4()}">
-//     <p class="workplace__item-text">${value}</p>
-//     <button class="workplace__item-delete" type="button" aria-label="delete current todo">
-//       <img class="workplace__item-image" src="images/dist/icons/delete.svg" alt="delete icon">
-//     </button>
-//   </li>
-//   `;
-// };
+const generate = (value) => {
+  return `
+  <li class="workplace__item" data-key="${uuidv4()}">
+    <label>
+      <input class="workplace__checkbox" type="checkbox" />
+      <p class="workplace__item-text">${value}</p>
+      <button
+        class="workplace__item-delete"
+        type="button"
+        aria-label="delete current todo"
+      >
+        <img
+          class="workplace__item-image"
+          src="images/dist/icons/delete.svg"
+                    alt="delete icon"
+        />
+      </button>
+    </label>
+  </li>
+  `;
+};
 
-// btn.addEventListener('click', () => {
-//   parent.insertAdjacentHTML('beforeend', generate('hello'));
-// });
+btn.addEventListener('click', () => {
+  parent.insertAdjacentHTML('beforeend', generate('hello'));
+});
