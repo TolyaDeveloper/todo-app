138ba873f60482dc81686ab1800cadd4deab7282
Anatoliy <kabantolyan@gmail.com>
2021-03-15 20:23:05 +0200

    local size + local animate

--- src/js/modules/accordion.js
+++ src/js/modules/accordion.js
@@ -1,3 +1,4 @@
+import { getLocal, setLocal } from '../utils';
 import { page } from './mode-switch';
 
 const input = document.querySelector('.menu__popup-accordion');
@@ -6,16 +7,36 @@ const range = document.querySelector('.menu__range-input');
 const animation = document.querySelector('.menu__popup-animation');
 const root = document.documentElement;
 
+if (getLocal('fontSize')) {
+  root.style.setProperty('--font-size-main', `${getLocal('fontSize')}px`);
+  range.value = getLocal('fontSize');
+}
+
+if (getLocal('rangeOpened')) {
+  input.setAttribute('checked', 'checked');
+  equalizerMenu.classList.add('active');
+}
+
+if (getLocal('animated')) {
+  page.dataset.transition = '';
+} else {
+  page.dataset.transition = 'no';
+  animation.removeAttribute('checked');
+}
+
 input.addEventListener('change', (e) => {
   if (e.target.checked) {
     equalizerMenu.classList.add('active');
+    setLocal('rangeOpened', true);
   } else {
     equalizerMenu.classList.remove('active');
+    localStorage.removeItem('rangeOpened');
   }
 });
 
 range.addEventListener('change', (e) => {
   const size = e.target.value;
+  setLocal('fontSize', size);
 
   root.style.setProperty('--font-size-main', `${size}px`);
 });
@@ -23,7 +44,9 @@ range.addEventListener('change', (e) => {
 animation.addEventListener('change', (e) => {
   if (!e.target.checked) {
     page.dataset.transition = 'no';
+    localStorage.removeItem('animated');
   } else {
     page.dataset.transition = '';
+    setLocal('animated', true);
   }
 });
